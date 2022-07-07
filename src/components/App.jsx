1ac1b045803126a4eb648461061d8f982895bc2b
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-07 16:07:59 +0200

    added styleConfig

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
+import { Component } from 'react';
 import { ContactForm } from './ContactForm';
 
-export const App = () => {
-  return (
-    <>
-      <ContactForm />
-    </>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <>
+        <ContactForm name={name} />
+      </>
+    );
+  }
+}
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,30 +2,29 @@ import { Component } from 'react';
 import { Formik } from 'formik';
 
 export class ContactForm extends Component {
-  state = {
-    name: '',
-  };
-
   handleSubmit = e => {};
 
   onChange = e => {
-    console.log(e);
-    this.setState({ value: e.target.value });
+    this.setState({ name: e.target.value });
   };
 
   render() {
-    const { name } = this.state;
+    const { name } = this.props;
+    console.log('this.props: ', this.props);
     return (
       <Formik onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={this.onChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+        <label htmlFor="name" style={{ display: 'block' }}>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.onChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
       </Formik>
     );
   }
