683303040d9a0c3a08d8022d629962eef94fd5f7
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-07-25 15:46:05 +0200

    removed handleChange function

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -37,10 +37,6 @@ export class ContactForm extends Component {
     number: '',
   };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.currentTarget.value });
-  };
-
   handleSubmit = e => {
     e.preventDefault();
     const contact = {
@@ -70,7 +66,9 @@ export class ContactForm extends Component {
               type="text"
               name="name"
               value={name}
-              onChange={this.handleChange}
+              onChange={e =>
+                this.setState({ [e.target.name]: e.currentTarget.value })
+              }
               placeholder="Enter name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -85,7 +83,9 @@ export class ContactForm extends Component {
               name="number"
               placeholder="Enter number"
               value={number}
-              onChange={this.handleChange}
+              onChange={e =>
+                this.setState({ [e.target.name]: e.currentTarget.value })
+              }
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
