c89eb63641232362caed102f8a16b2716a9bf2e7
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-09-08 15:42:58 +0200

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,82 @@ import { ContactForm } from './components/ContactForm';
 import { ContactList } from './components/ContactList';
 import { Filter } from './components/Filter';
 
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(contacts);
+
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   addContact = contact => {
+//     if (this.state.contacts.find(({ name }) => contact.name === name))
+//       return alert(`name is already in contacts`);
+//     if (this.state.contacts.find(({ number }) => contact.number === number))
+//       return alert(`number is already in contacts`);
+
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, contact],
+//     }));
+//   };
+
+//   deleteContact = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const form = e.currentTarget;
+
+//     form.reset();
+//   };
+
+//   handleSearch = e => {
+//     const searchQuery = e.currentTarget.value;
+//     this.setState({ filter: searchQuery });
+//   };
+
+//   render() {
+//     const { contacts, filter } = this.state;
+//     const filteredContacts = contacts.filter(({ name }) =>
+//       name.toLowerCase().includes(filter.toLowerCase())
+//     );
+
+//     return (
+//       <>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.addContact} />
+//         <h2>Contacts</h2>
+//         <Filter filter={filter} handleSearch={this.handleSearch} />
+//         <ContactList
+//           contacts={filteredContacts}
+//           deleteContact={this.deleteContact}
+//         />
+//       </>
+//     );
+//   }
+// }
+
 export class App extends Component {
   state = {
     contacts: [
