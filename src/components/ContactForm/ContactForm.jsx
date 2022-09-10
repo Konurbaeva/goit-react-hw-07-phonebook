89985b3720c643b93d3a2e9baff6b69ee4d142af
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-09-10 18:28:55 +0200

    adjusting styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -35,7 +35,7 @@ export function ContactForm({ onFormSubmit }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       Name
       <Input
         value={name}
@@ -59,7 +59,7 @@ export function ContactForm({ onFormSubmit }) {
         required
       />
       <Button type="submit">Add contact</Button>
-    </form>
+    </Form>
   );
 }
 
@@ -67,21 +67,27 @@ ContactForm.propTypes = {
   onFormSubmit: PropTypes.func.isRequired,
 };
 
+const Form = styled.form`
+  border: ${props => props.theme.borders.normal} black;
+  padding: 10px;
+  border-radius: 5px;
+  width: 320px;
+`;
 const Input = styled.input`
   padding: 0.5em;
   margin: 0.5em;
-  color: ${props => props.inputColor || 'palevioletred'};
-  background: pink;
+  color: ${props => props.theme.colors.primary};
+  background: ${props => props.theme.colors.accent};
   border: none;
   border-radius: 3px;
 `;
 
 const Button = styled.button`
-  background: ${props => (props.primary ? 'palevioletred' : 'white')};
-  color: ${props => (props.primary ? 'white' : 'palevioletred')};
-  font-size: 1em;
+  font-size: 0.5em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  padding: 0.5em;
+  color: ${props => props.theme.colors.primary};
+  border: ${props => props.theme.colors.primary} palevioletred;
   border-radius: 3px;
 `;
