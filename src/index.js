af24a40bb5c60bae39a45f698dc7c24e8457da49
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-10-16 20:47:38 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { App } from 'App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styleConfig/theme';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
+     <Provider store={store}>
     <App />
+    </Provider>
   </ThemeProvider>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+export const myValueSlice = createSlice({
+  name: 'myValue',
+  initialState: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  ],
+  reducers: {
+    addNewContact(state, action) {
+      return [...state, action.payload];
+    },
+    deleteContact(state, action) {
+      return state.filter(contact => contact.id !== action.payload);
+    },
+  },
+});
+
+export const { addNewContact, deleteContact } = myValueSlice.actions;
+
+console.log('myValueSlice.actions ', myValueSlice.actions);
+
+
+const store = configureStore({
+  reducer: {
+    myValue: myValueSlice.reducer,
+  },
+});
+
+export default store;
